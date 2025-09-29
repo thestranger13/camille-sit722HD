08d66284efa8c66a3fedcfb07ef11cabdf150103
Camille Ang <S223646635@deakin.edu.au>
2025-09-29 11:52:07 +1000

    Integrate Security Testing

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,8 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+// Import the pluginSecurity
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  // base JS recommended rules
+  js.configs.recommended,
+  // project rules 
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+
+    // register plugin under security 
+    plugins: {
+      security: pluginSecurity,
+    },
+    // use the recommended rules 
+    rules: {
+      ...(pluginSecurity.configs?.recommended?.rules ?? {}),
+    },
+  },
+
+    {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
 ]);
